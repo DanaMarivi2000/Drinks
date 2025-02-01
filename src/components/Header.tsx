0f5c14fa65ee083080155bcd1fa0e9d80761f3fe
Dana Gamez <danagamezdbg@gmail.com>
2025-01-31 22:59:10 -0600

    ADD form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
-import { Link, NavLink } from "react-router-dom"
+import { useMemo } from "react"
+import {NavLink, useLocation } from "react-router-dom"
 const Header = () => {
+
+  const{pathname}=useLocation()
+  const location=useMemo(()=>pathname==="/",[pathname])
+  console.log(location)
   return (
-    <header className='header-principal'>
+    <header className={location?'header-principal':'header-alternative'}>
       <div className='header-principal-firstcontainer'>
         <div className='header-principal-secondcontainer'>
             <div className="logo">
@@ -13,6 +18,21 @@ const Header = () => {
                
             </nav>
         </div>
+        {location&&(
+          <form className="formulario">
+            <div className="formulario__container">
+              <label htmlFor="ingredient" className="formulario__label">
+                Nombre o Ingredientes
+              </label>
+              <input type="text" placeholder="Ej. Vodka" className="formulario__input" id="ingredient"/>
+              <label htmlFor="category" className="formulario__label">Categoría</label>
+              <select className="formulario__select" id="category">
+                <option value="">-- Seleccione --</option>
+              </select>
+            </div>
+            <input type="submit" value='Buscar Recetas' className="formulario__button" />
+          </form>
+        )}
       </div>
     </header>
   )
